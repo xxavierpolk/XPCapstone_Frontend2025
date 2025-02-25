f09708a85049cd26502d3576f966ec0675d0db0a
Xxavier Polk <xxavier@xxavierpolk.com>
2025-02-25 00:56:03 -0500

    Protected Routes V1

---
+++ src/components/ProtectedRoutes.jsx
@@ -0,0 +1,9 @@
+import { Outlet } from "react-router-dom"
+import { useAuth } from "../context/auth_context"
+
+
+export default function ProtectedRoutes() {
+    const { cookies } = useAuth();
+
+    return cookies.token ? <Outlet /> : <h1>AYE! HOLD IT! YOU AINT SUPPOSED TO BE HERE.</h1>
+}
