d8deffe3d53bccec7aeff7c496be02a31b1624ae
Xxavier Polk <xxavier@xxavierpolk.com>
2025-02-25 08:24:35 -0500

    Login Form Logic v1

--- src/components/login_form/Index.jsx
+++ src/components/login_form/Index.jsx
@@ -1,4 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../context/auth/auth_context';
+import { useState } from 'react';
+
+
 const LoginForm = ({ setNewUser }) => {
+  const nav = useNavigate(); // Initialized useNavigate into variable
+  const { login } = useAuth(); // Destructure the login from context
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  function handleChange(event) {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    await login(formData);
+    nav('/dashboard');
+  }
+
   const handleClick = () => {
     setNewUser(true);
   };
@@ -6,26 +28,54 @@ const LoginForm = ({ setNewUser }) => {
   return (
     <div className='forms'>
       <h2>Login</h2>
-      <form autoComplete='off'>
+      <form autoComplete='off' onSubmit={handleSubmit}>
+
         <label htmlFor='email'>Email: </label>
-        <input type='email' id='email' name='email' placeholder='Email' />
+
+        <input 
+          onChange={handleChange}
+          type='email' 
+          id='email' 
+          name='email' 
+          placeholder='Email' />
+
         <label htmlFor='password'>Password: </label>
-        <input
+
+        <input 
+          onChange={handleChange}
           type='password'
           id='password'
           name='password'
           placeholder='Password'
           minLength='6'
         />
-        <button type='submit' onClick={handleClick}>
+
+        <button type='submit' >
           Log In
         </button>
+
       </form>
+
       <p>
         Dont have an account? <button onClick={handleClick}>Sign Up</button>
       </p>
+
     </div>
   );
 };
 
 export default LoginForm;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
