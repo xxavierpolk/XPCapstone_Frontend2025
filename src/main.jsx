cca1375f510aa2d3d5b4dd58d6b792f9054b76e1
Xxavier Polk <xxavier@xxavierpolk.com>
2025-02-25 00:08:09 -0500

    Wrapped App Provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import AppProvider from './context/app_context.jsx';
 import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <AppProvider>
     <Router>
       <App />
     </Router>
+    </AppProvider>
   </React.StrictMode>
 );
