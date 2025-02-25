eb41f4f684e9318c5bbffc945526956cc8189016
Xxavier Polk <xxavier@xxavierpolk.com>
2025-02-25 01:54:22 -0500

    Logout Created & Tested Complete

--- src/pages/dashboard/Index.jsx
+++ src/pages/dashboard/Index.jsx
@@ -1,7 +1,21 @@
+import { useAuth } from "../../context/auth/auth_context";
+import { useNavigate } from "react-router-dom";
+
+
+
 const Dashboard = () => {
+  const nav = useNavigate();
+  const { logout } = useAuth();
+
+  function handleLogout() {
+    logout();
+    nav("/auth");
+  }
+
   return (
     <div>
       <h1>Only Users Should See This Page</h1>
+      <button onClick={handleLogout}>Logout</button>
     </div>
   );
 };
