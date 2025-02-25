acae981d7489e6f9e813c6e6fa7a332b21fcb5ec
Xxavier Polk <xxavier@xxavierpolk.com>
2025-02-24 23:52:36 -0500

    Context Made

--- src/context/auth/auth_context.jsx
+++ src/context/auth/auth_context.jsx
@@ -2,69 +2,159 @@
 import { createContext, useContext, useMemo } from 'react';
 import { useCookies } from 'react-cookie';
 
-// Create context
+
 const AuthContext = createContext();
 
-// Create functional component AuthProvider. Destructures children as its sole parameter
-export default function AuthProvider({ children}) {
-    const [cookies, setCookie, removeCookie] = useCookies(['token']);
+export default function AuthProvider({ children }) {
+  // Use cookies to keep track and manage cookies
+  const [cookies, setCookies, removeCookie] = useCookies();
+
+  //   Login Function
+  async function login(formData) {
+    try {
+      // Make post request to backend with formData
+      const response = await fetch('http://localhost:3000/api/auth', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+
+    });
+        const data = await response.json();
+
+    // Take the token from the response and store it in a cookie
+        setCookies('token', data.token);
+        return data;
+        } catch (err) {
+            console.error(err);
+    }
+  }
+
+  // register Function
+  async function signUp(formData) {
+    try {
+      // Make post request to backend with formData
+      const response = await fetch('http://localhost:3000/api/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+
+    });
+    const data = await response.json();
+
+    // Take the token from the response and store it in a cookie
+        setCookies('token', data.token);
+        return data;
+         } catch (err) {
+      console.error(err);
+    }
+  } 
+
+  // Logout Function
+  function logout() {
+    ['token'].forEach((obj) => removeCookie(obj));
+  }
+
+  const value = useMemo(
+    () => ({
+      cookies,
+      login,
+      logout,
+      signUp,
+    }),
+    [cookies]
+  );
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+
+// Cheeky function so we don't have import useContext in every file or everytime i want to use Auth
+export function useAuth() {
+  return useContext(AuthContext);
+}
 
 
-    // Login Function
-    async function login(formData) {
-        try {
-            // Make a POST request to the backend with the form data
-            const response = await fetch('http://localhost:3000/api/auth', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(formData)
+
+
+
+
+// // Imports
+// import { createContext, useContext, useMemo } from 'react';
+// import { useCookies } from 'react-cookie';
+
+// // Create context
+// const AuthContext = createContext();
+
+// // Create functional component AuthProvider. Destructures children as its sole parameter
+// export default function AuthProvider({ children}) {
+//     const [cookies, setCookie, removeCookie] = useCookies(['token']);
+
+
+//     // Login Function
+//     async function login(formData) {
+//         try {
+//             // Make a POST request to the backend with the form data
+        //     const response = await fetch('http://localhost:3000/api/auth', {
+        //         method: 'POST',
+        //         headers: {
+        //             'Content-Type': 'application/json'
+        //         },
+        //         body: JSON.stringify(formData)
     
-            });
-            const data = await response.json();
+        //     });
+        //     const data = await response.json();
 
-            // Take the token from the response and store it in a cookie
-            setCookie('token', data.token);
-            return data;
-        } catch (error) {
-            console.error(error);
-            return { errors: [{ msg: 'Server Error' }] };
-        }
+        //     // Take the token from the response and store it in a cookie
+        //     setCookie('token', data.token);
+        //     return data;
+        // } catch (error) {
+//             console.error(error);
+//             return { errors: [{ msg: 'Server Error' }] };
+//         }
         
 
-    // Register Function
-    async function signUp(formData) {
-        try {
-            // Make a POST request to the backend with the form data
-            const response = await fetch('http://localhost:3000/api/users', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(formData)
+//     // Register Function
+//     async function signUp(formData) {
+//         try {
+//             // Make a POST request to the backend with the form data
+        //     const response = await fetch('http://localhost:3000/api/users', {
+        //         method: 'POST',
+        //         headers: {
+        //             'Content-Type': 'application/json'
+        //         },
+        //         body: JSON.stringify(formData)
     
-            });
-            const data = await response.json();
+        //     });
+        //     const data = await response.json();
 
-            // Take the token from the response and store it in a cookie
-            setCookie('token', data.token);
-            return data;
-        } catch (error) {
-            console.error(error);
-            return { errors: [{ msg: 'Server Error' }] };
-        }
-    }
+        //     // Take the token from the response and store it in a cookie
+        //     setCookie('token', data.token);
+        //     return data;
+        // } catch (error) {
+//             console.error(error);
+//             return { errors: [{ msg: 'Server Error' }] };
+//         }
+//     }
 
-    // Logout Function
-    function logout() {
-        ['token'].forEach((cookie) => {
-            removeCookie(cookie);
-        });
-    }
+//     // Logout Function
+//     function logout() {
+//         ['token'].forEach((cookie) => {
+//             removeCookie(cookie);
+//         });
+//     }
 
+
+//     const value = useMemo(() => ({
+//         cookies,
+//         login,
+//         logout,
+//         signUp
+//     }), [cookies]);
     
 
-    return <AuthContext.Provider>{children}</AuthContext.Provider>;
-}
-}
+//     return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+// }
+// }
